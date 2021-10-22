d6e84eae3bed6c4b3b4ff2d6de4e0e1fa4bbfdab
matheus severo goerg <37753453+matheusseverogoergdev@users.noreply.github.com>
2021-10-22 13:03:57 -0300

    Atualização dos arquivos desatualizados que estavam dando erro nas nossas máquinas.

--- src/br/com/senactech/ProjetoIntegradorVCc/model/Usuario.java
+++ src/br/com/senactech/ProjetoIntegradorVCc/model/Usuario.java
@@ -10,18 +10,46 @@ package br.com.senactech.ProjetoIntegradorVCc.model;
  * @author User
  */
 public class Usuario {
+    private int idUsuario;
+    private boolean status;
     private String endereco;
     private String nome;
     private String telefone;
+    private String email;
+    private String cep;
+    private String cpf;
+    private String senha;
     
     public Usuario() {
     
     }
 
-    public Usuario(String endereco, String nome, String telefone) {
+    public Usuario(int idUsuario, boolean status, String endereco, String nome, String telefone, String email, String cep, String cpf, String senha) {
+        this.idUsuario = idUsuario;
+        this.status = status;
         this.endereco = endereco;
         this.nome = nome;
         this.telefone = telefone;
+        this.email = email;
+        this.cep = cep;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 
     public String getEndereco() {
@@ -48,11 +76,41 @@ public class Usuario {
         this.telefone = telefone;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
     @Override
     public String toString() {
-        return "Usuario{" + "endereco=" + endereco + ", nome=" + nome + ", telefone=" + telefone + '}';
+        return "Usuario{" + "idUsuario=" + idUsuario + ", status=" + status + ", endereco=" + endereco + ", nome=" + nome + ", telefone=" + telefone + ", email=" + email + ", cep=" + cep + ", cpf=" + cpf + ", senha=" + senha + '}';
     }
-    
-    
     
 }
